759906531f94c6ed4c33942aa36e581d826e4909
bgskoro21 <bagaskara148@gmail.com>
2023-09-21 08:37:39 +0700

    first commit

---
+++ test.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+func main(){
+	// var warnaLampu = "merah";
+
+	// if warnaLampu == "merah"{
+	// 	fmt.Println("Halo Merah")
+	// }else{
+	// 	fmt.Println("Halo lampu")
+	// }
+
+	// Switch case
+	// switch warnaLampu{
+	// case "merah":
+	// 	fmt.Println("berhenti")
+	// case "kuning":
+	// 	fmt.Println("hati-hati")
+	// default:
+	// 	fmt.Println("maju")
+	// }
+
+	// looping for
+	// for i := 1; i < 5; i++ {
+	// 	fmt.Println("Angka",i)
+	// }
+
+	// looping while
+	// var i = 0;
+	// for i < 5{
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+
+	// loop do while
+	// var i = 0
+	// for{
+	// 	fmt.Println("angka",i)
+
+	// 	i++ 
+	// 	if i == 5{
+	// 		break
+	// 	}
+	// }
+
+	// array
+	// var names[3]string
+	// names[0] = "Bagaskara"
+	// names[1] = "Bagaskoro"
+	// names[2] = "Bagas Aja"
+	var names = [...]string{
+		"Bagaskara",
+		"Bagaskoro",
+		"Bagas Aja",
+	}
+	
+	fmt.Println(names[0], names[1], names[2])
+}
